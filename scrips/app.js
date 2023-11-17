5d866d8644e109f88c2d8fe8ad0e4fae49109705
Maty1389 <151097134+Maty1389@users.noreply.github.com>
2023-11-16 22:46:01 -0300

    Add files via upload

---
+++ scrips/app.js
@@ -0,0 +1,17 @@
+const arriba = document.getElementById("arriba")
+
+arriba.addEventListener("click", () => {
+Window.scrollTo( 0,0);
+});
+
+buttonUp = document.getElementById("arriba");
+
+window.onscroll = function() {
+    var scroll = document.documentElement.scrollTop;
+
+    if (scroll > 500) {
+        buttonUp.style.transform = "scale(1)";
+    } else if (scroll < 500) {
+        buttonUp.style.transform  = "scale(0)";
+    }
+}
